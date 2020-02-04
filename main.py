ad2bf87557057a2e5d2d2f7ef340e7ec289a2d46
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-04 08:21:47 -0300

    Add --file and --epsilon command line args

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
-import mdp_graph
+import argparse
+import mdp_graph as mg
+import numpy as np
+from pprint import pprint
+from lao import lao
 
 
 def read_json(file_name):
@@ -7,12 +11,27 @@ def read_json(file_name):
         return json.load(json_data)
 
 
-def lao(s0, mdp):
-    pass
+DEFAULT_FILE_INPUT = './env-paper.json'
+DEFAULT_EPSILON = 1e-3
 
+parser = argparse.ArgumentParser(description='LAO* algorithm implementation.')
 
-file_input = './env-paper.json'
-mdp = read_json(file_input)
-# print(mdp)
+parser.add_argument('--file', dest='file_input',
+                    default=DEFAULT_FILE_INPUT, help="Environment JSON file used as input (default: %s)" % DEFAULT_FILE_INPUT)
+parser.add_argument('--epsilon', dest='epsilon',
+                    default=DEFAULT_EPSILON, help="Epsilon used for convergence (default: %s)" % str(DEFAULT_EPSILON))
 
-lao(1, mdp)
+args = parser.parse_args()
+
+mdp = read_json(args.file_input)
+A = mg.get_actions(mdp)
+S = list(mdp.keys())
+V_i = {S[i]: i for i in range(len(S))}
+
+heuristic = np.array([3.0, 2.0, 1.0, 0.0, 1.0, 2.0, 3.0, 4.0])
+
+V, pi = lao('1', heuristic, V_i, S, A,
+            mg.init_graph(mdp), epsilon=args.epsilon)
+
+print('V: ', V)
+print('pi: ', pi)
