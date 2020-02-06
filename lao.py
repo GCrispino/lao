43b2e2a178ede170ced6361839adc62cd9e77aa1
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-06 18:20:06 -0300

    Finish implementing convergence test

--- lao.py
+++ lao.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mdp_graph as mg
 
 
@@ -12,12 +13,39 @@ def lao(s0, heuristic, V_i, pi, S, A, mdp, epsilon=1e-3):
     i = 0
     unexpanded = mg.get_unexpanded_states(mdp, bpsg)
     V = heuristic
-    while len(unexpanded) > 0:
-        s = unexpanded[0]
-        explicit_graph, mdp = mg.expand_state(s, mdp, explicit_graph)
-        Z = mg.find_ancestors(s, bpsg) + [s]
-        V, pi = mg.value_iteration(V, V_i, pi, A, Z, mdp, epsilon=epsilon)
-        bpsg = mg.update_partial_solution(pi, S, bpsg, mdp)
-        unexpanded = mg.get_unexpanded_states(mdp, bpsg)
-        i += 1
+    while True:
+        while len(unexpanded) > 0:
+            s = unexpanded[0]
+            explicit_graph, mdp = mg.expand_state(s, mdp, explicit_graph)
+            Z = mg.find_ancestors(s, bpsg) + [s]
+            V, pi = mg.value_iteration(V, V_i, pi, A, Z, mdp, epsilon=epsilon)
+            bpsg = mg.update_partial_solution(pi, S, bpsg, mdp)
+            unexpanded = mg.get_unexpanded_states(mdp, bpsg)
+            i += 1
+        V, pi, converged = convergence_test(
+            V, V_i, pi, A, Z, mdp, epsilon=epsilon)
+
+        if converged:
+            break
+        else:
+            bpsg = mg.update_partial_solution(pi, S, bpsg, mdp)
+            unexpanded = mg.get_unexpanded_states(mdp, bpsg)
     return V, pi
+
+
+def convergence_test(V, V_i, pi, A, Z, mdp, c=1, epsilon=1e-3):
+    i = 0
+    while(True):
+        old_pi = np.array(pi)
+        V_, pi = mg.bellman(V, V_i, pi, A, Z, mdp, c)
+        pi_ = np.array(pi)
+        different_actions = pi_[pi_ != old_pi]
+
+        # converged
+        if np.linalg.norm(V_ - V, np.inf) < epsilon:
+            return V_, pi, True
+        if len(different_actions) > 0:
+            return V_, pi, False
+
+        V = V_
+        i += 1
---
+++ test/test_lao.py
@@ -0,0 +1,170 @@
+import pytest
+import unittest
+import numpy as np
+import mdp_graph as mg
+import lao
+from unittest.mock import patch
+"""
+
+    Testar função convergence_test
+    heurística:
+        | 2 | 1.5 | 0 |
+        | 1 | 2   | 1 |
+    política inicial:
+        | S | E | 0 |
+        | E | E | N |
+
+"""
+graph = {
+    "1": {
+        "goal": False,
+        "Adj": [
+            {
+                "name": "1",
+                "A": {
+                    "N": 1,
+                    "S": 0.5,
+                    "E": 0.5
+                }
+            },
+            {
+                "name": "2",
+                "A": {"E": 0.5}
+            },
+            {
+                "name": "4",
+                "A": {"S": 0.5}
+            }
+        ]
+    },
+    "2": {
+        "goal": False,
+        "Adj": [
+            {
+                "name": "2",
+                "A": {
+                    "N": 1,
+                    "S": 0.5,
+                    "E": 0.5
+                }
+            },
+            {
+                "name": "3",
+                "A": {"E": 0.5}
+            },
+            {
+                "name": "5",
+                "A": {"S": 0.5}
+            }
+        ]
+    },
+    "3": {
+        "goal": True,
+        "Adj": [
+            {
+                "name": "3",
+                "A": {
+                    "N": 1,
+                    "S": 1,
+                    "E": 1
+                }
+            }
+        ]
+    },
+    "4": {
+        "goal": False,
+        "Adj": [
+            {
+                "name": "1",
+                "A": {"N": 1}
+            },
+            {
+                "name": "4",
+                "A": {"S": 1}
+            },
+            {
+                "name": "5",
+                "A": {"E": 1}
+            }
+        ]
+    },
+    "5": {
+        "goal": False,
+        "Adj": [
+            {
+                "name": "2",
+                "A": {"N": 1}
+            },
+            {
+                "name": "5",
+                "A": {"S": 1}
+            },
+            {
+                "name": "6",
+                "A": {"E": 1}
+            }
+        ]
+    },
+    "6": {
+        "goal": False,
+        "Adj": [
+            {
+                "name": "3",
+                "A": {"N": 1}
+            },
+            {
+                "name": "6",
+                "A": {"S": 1, "E": 1}
+            }
+        ]
+    }
+}
+
+A = ['N', 'S', 'E']
+S = list(graph.keys())
+V_i = {S[i]: i for i in range(len(S))}
+
+ct = lao.convergence_test
+
+
+def mocked_convergence_test(obj):
+    def fn(V, V_i, pi, A, Z, mdp, c=1, epsilon=1e-3):
+        if obj['i'] == 0:
+            new_pi = np.array(['S', 'E', None, None, None, None])
+            obj['i'] += 1
+            return V, new_pi, False
+
+        obj['i'] += 1
+        return ct(V, V_i, pi, A, Z, mdp, c=c, epsilon=epsilon)
+    return fn
+
+
+class TestLAO(unittest.TestCase):
+    def test_convergence_test(self):
+        V = np.array([2, 1.5, 0, 1, 2, 1])
+        pi = np.array(['S', 'E', None, 'E', 'E', 'N'])
+        Z = ['1', '4', '5', '6']
+        V_, pi_, converged = lao.convergence_test(V, V_i, pi, A, Z, graph)
+
+        self.assertListEqual(pi_.tolist(), ['S', 'E', None, 'S', 'E', 'N'])
+        assert not converged
+
+    def test_lao(self):
+        V = np.array([2, 1.5, 0, 1, 2, 1])
+        pi = np.array([None] * len(S))
+        V_, pi_ = lao.lao('1', V, V_i, pi, S, A, mg.init_graph(graph))
+        self.assertListEqual(pi_.tolist(), ['E', 'E', None, 'E', 'E', 'N'])
+
+    def test_lao_2(self):
+        V = np.array([2.0, 1, 0, 3, 2, 1])
+        pi = np.array([None] * len(S))
+        V_, pi_ = lao.lao('1', V, V_i, pi, S, A, mg.init_graph(graph))
+        self.assertListEqual(pi_.tolist(), ['E', 'E', None, None, None, None])
+
+    @patch('lao.convergence_test', new=mocked_convergence_test({'i': 0}))
+    def test_lao_with_convergence_test(self):
+        V = np.array([2.0, 1, 0, 3, 2, 1])
+        pi = np.array([None] * len(S))
+        V_, pi_ = lao.lao('1', V, V_i, pi, S, A, mg.init_graph(graph))
+
+        self.assertListEqual(pi_.tolist(), ['E', 'E', None, 'E', 'E', 'N'])
