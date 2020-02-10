1cf01ffe728f86cf51214511700a3a655c3d842a
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-10 17:43:25 -0300

    Add algorithm as cl argument and move argparsing to utils file

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import argparse
 import numpy as np
 import mdp_graph as mg
-from lao import lao
-from utils import read_json
+from lao import lao, ilao
+from utils import read_json, parse_args
 
-
-DEFAULT_FILE_INPUT = './env-paper.json'
-DEFAULT_EPSILON = 1e-3
-
-parser = argparse.ArgumentParser(description='LAO* algorithm implementation.')
-
-parser.add_argument('--file', dest='file_input',
-                    default=DEFAULT_FILE_INPUT,
-                    help="Environment JSON file used as input (default: %s)" % DEFAULT_FILE_INPUT)
-parser.add_argument('--epsilon', dest='epsilon', type=float,
-                    default=DEFAULT_EPSILON,
-                    help="Epsilon used for convergence (default: %s)" % str(DEFAULT_EPSILON))
-
-args = parser.parse_args()
+args = parse_args()
 
 mdp = read_json(args.file_input)
 A = mg.get_actions(mdp)
@@ -26,9 +12,14 @@ pi = np.array([None] * len(S))
 V_i = {S[i]: i for i in range(len(S))}
 
 heuristic = np.fromiter((map(lambda s: s['heuristic'], mdp.values())), float)
+V = None
 
-V, pi = lao('1', heuristic, V_i, pi, S, A,
-            mg.init_graph(mdp), epsilon=args.epsilon)
+if args.algorithm == 'lao':
+    V, pi = lao('1', heuristic, V_i, pi, S, A,
+                mg.init_graph(mdp), epsilon=args.epsilon)
+elif args.algorithm == 'ilao':
+    V, pi = ilao('1', heuristic, V_i, pi, S, A,
+                 mg.init_graph(mdp), epsilon=args.epsilon)
 
 print('V: ', V)
 print('pi: ', pi)
--- utils.py
+++ utils.py
@@ -1,6 +1,30 @@
+import argparse
 import json
 
 
 def read_json(file_name):
     with open(file_name) as json_data:
         return json.load(json_data)
+
+
+DEFAULT_FILE_INPUT = './env-paper.json'
+DEFAULT_EPSILON = 1e-3
+DEFAULT_ALGORITHM = 'lao'
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(
+        description='LAO* algorithm implementation.')
+
+    parser.add_argument('--file', dest='file_input',
+                        default=DEFAULT_FILE_INPUT,
+                        help="Environment JSON file used as input (default: %s)" % DEFAULT_FILE_INPUT)
+    parser.add_argument('--epsilon', dest='epsilon', type=float,
+                        default=DEFAULT_EPSILON,
+                        help="Epsilon used for convergence (default: %s)" % str(DEFAULT_EPSILON))
+    parser.add_argument('--algorithm', dest='algorithm', choices=['lao', 'ilao'],
+                        default=DEFAULT_ALGORITHM,
+                        help="Algorithm to run (default: %s)" % DEFAULT_ALGORITHM)
+
+    return parser.parse_args()
