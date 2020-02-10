4cd5368637e7eca7c0bfbf840ea55a01805d431f
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-10 17:34:59 -0300

    Implement ILAO* algorithm

--- lao.py
+++ lao.py
@@ -30,6 +30,38 @@ def lao(s0, heuristic, V_i, pi, S, A, mdp, epsilon=1e-3):
     return V, pi
 
 
+def ilao(s0, heuristic, V_i, pi, S, A, mdp, epsilon=1e-3):
+    bpsg = {s0: {"Adj": []}}
+    explicit_graph = deepcopy(bpsg)
+
+    unexpanded = mg.get_unexpanded_states(mdp, bpsg)
+    V = heuristic
+    while True:
+        while len(unexpanded) > 0:
+            s = unexpanded[0]
+
+            def visit(s):
+                nonlocal explicit_graph, bpsg, mdp, V, pi, V_i, A
+                if not mdp[s]['goal'] and not mdp[s]['expanded']:
+                    explicit_graph, mdp = mg.expand_state(
+                        s, mdp, explicit_graph)
+                # run bellman backup
+                V, pi = mg.bellman(V, V_i, pi, A, [s], mdp)
+            mg.dfs(bpsg, visit)
+            bpsg = mg.update_partial_solution(pi, S, bpsg, mdp)
+            unexpanded = mg.get_unexpanded_states(mdp, bpsg)
+
+        V, pi, converged = convergence_test(
+            V, V_i, pi, A, list(bpsg.keys()), mdp, epsilon=epsilon)
+
+        if converged:
+            break
+        # else
+        bpsg = mg.update_partial_solution(pi, S, bpsg, mdp)
+        unexpanded = mg.get_unexpanded_states(mdp, bpsg)
+    return V, pi
+
+
 def convergence_test(V, V_i, pi, A, Z, mdp, c=1, epsilon=1e-3):
     i = 0
     while True:
